4e420fbeb66e7267145a2c58d1c487cb62dcdf7f
hexinzhe <hexinzhe@live.com>
2016-04-25 10:35:44 +0800

    测试

---
+++ src/enums/Speed.java
@@ -0,0 +1,17 @@
+package enums;
+
+public enum Speed {
+	MIN(3),//由于每一格算两米，所以这个地方要除以2，最低速是6米每秒
+	NORMAL_1(4),
+	NORMAL_2(5), 
+	NORMAL_3(6),
+	NORMAL_4(7),
+	MAX(8);
+	private int value = 0;
+	private Speed(int value) {
+		this.value = value;
+	}
+	public int value() {
+		return this.value;
+	}
+}
